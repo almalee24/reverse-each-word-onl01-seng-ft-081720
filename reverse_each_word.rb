f10c410f3dee56b77ab8ed078c53d6508a5aad2f
Alma Flores <flores.almalee@gmail.com>
2020-08-18 23:07:24 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(str)
+  new_array = []
+  str.split(" ").each do |word|
+    new_array << word.reverse
+  end
+  new_array.join(' ')
+end
+
+def reverse_each_word(str)
+  new_array = []
+  str.split(" ").collect do |word|
+    new_array << word.reverse
+  end
+  new_array.join(' ')
+end
