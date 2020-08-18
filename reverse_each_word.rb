0978fca28345455564fd596b4887d178bbade2fe
Alma Flores <flores.almalee@gmail.com>
2020-08-18 22:51:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(str)
-  array = str.split(',')
   new_array = []
-  array.each do |word|
+  str.split(',').each do |word|
     new_array << word.reverse
   end
   new_array.join(' ')
